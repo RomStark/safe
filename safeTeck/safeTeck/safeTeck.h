d037b9e6cc62023e653e835c5f21f9f8d20a4840
RomStark <rgtarkovskiy@edu.hse.ru>
2023-10-17 15:48:34 +0300

    init commit

---
+++ safeTeck/safeTeck/safeTeck.h
@@ -0,0 +1,18 @@
+//
+//  safeTeck.h
+//  safeTeck
+//
+//  Created by Al Stark on 17.10.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for safeTeck.
+FOUNDATION_EXPORT double safeTeckVersionNumber;
+
+//! Project version string for safeTeck.
+FOUNDATION_EXPORT const unsigned char safeTeckVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <safeTeck/PublicHeader.h>
+
+
